fd9cb923ec27b0a8d76db04fccfce950717a4d4b
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-04-11 22:07:24 +0500

    functions

--- app.js
+++ app.js
@@ -10,3 +10,23 @@
 //     console.log(tableOf + " * " + num + " = " + tableOf * num);
 // }
 //Functions
+//1st
+// function sum(){
+//     var a = 10, b = 20;
+//     total = a + b;
+//     console.log(total);
+// }
+// sum();
+//2nd
+// function sum(a,b){
+//     return total = a+b;
+// }
+// var sum1 = sum(5,15);
+// var sum2 = sum(15,15);
+// console.log(sum1 > sum2);
+//3rd Anonymous Function
+// var sum = function(a,b){
+//     return total = a+b;
+// }
+// console.log(sum(5,55));
+
