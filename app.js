06dc6b8f29253ea51b3091db6d07d1aadf0d0b48
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-04-11 22:02:45 +0500

    for loop

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+// score = 10;
+// if(score = 10){
+//     console.log("good score");
+// }
+// console.log((score = 10) ? "Good Score" : "Bad Score");
+//table of any number
 
-
-score = 10;
-if(score = 10){
-    console.log("good score");
-
-
-
-
-}
-
-console.log((score = 10) ? "Good Score" : "Bad Score");
+// for(var num = 1 ; num<=10; num++){
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + num + " = " + tableOf * num);
+// }
+//Functions
