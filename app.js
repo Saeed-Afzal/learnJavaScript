68099fad5ce40a4bfb279d7d98a22c7fc5b62d63
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-05-30 11:50:38 +0500

    function currying

--- app.js
+++ app.js
@@ -334,3 +334,20 @@
 
 // }
 // fun2();
+
+
+//Function Currying
+// function sum(num1){
+//     // console.log(num1);
+//     return function(num2){
+//         return function(num3){
+//             console.log(num1, num2 , num3);
+
+//             console.log(num1 + num2 + num3);
+            
+//         }
+//     }
+// }
+
+// sum(5)(3)(8)
+//Function Currying
