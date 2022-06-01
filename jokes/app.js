9154c8e742e2ebe324a926560503af5bdfe661e5
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-06-01 10:09:00 +0500

    async await

--- jokes/app.js
+++ jokes/app.js
@@ -4,29 +4,56 @@
 
 const jokes = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
+//By using Promise fetch then
+// const generateJokes = () => {
+//     //api se html me data araha hai to hmen header set krna hoga
+//     const setHeader = {
+//         headers : {
+//             Accept : "application/json"
+//         }
+//     }
 
-const generateJokes = () => {
-    //api se html me data araha hai to hmen header set krna hoga
-    const setHeader = {
-        headers : {
-            Accept : "application/json"
+// //fetch and then
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//     .then((res) => res.json()
+//     )
+//     .then((data) => {
+//         jokes.innerHTML = data.joke;
+//     })
+    
+    
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// }
+
+
+
+
+
+//By using async await
+const generateJokes = async () => {
+    try{
+
+        //api se html me data araha hai to hmen header set krna hoga
+        const setHeader = {
+            headers : {
+                Accept : "application/json"
+            }
         }
+    
+    
+      const res = await fetch('https://icanhazdadjoke.com', setHeader)
+      const data = await res.json();
+      jokes.innerHTML = data.joke;
+    }
+    catch(err){
+        console.log(`The Error is ${err}`);
     }
 
-//fetch and then
-    fetch('https://icanhazdadjoke.com', setHeader)
-    .then((res) => res.json()
-    )
-    .then((data) => {
-        jokes.innerHTML = data.joke;
-    })
-    
-    
-    .catch((err) => {
-        console.log(err);
-    })
-}
 
+
+    }
 
 
 jokeBtn.addEventListener('click', generateJokes);
