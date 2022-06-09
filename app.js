9d0ea5a8e2e63d435955f0f780a626b2a5824024
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-06-09 11:41:05 +0500

    var, let use in loops

--- app.js
+++ app.js
@@ -375,3 +375,14 @@
 // arr.length = 0
 // console.log("Now the array is empty :" + arr); // []
 //how to empty array
+
+// var and let
+//In the second loop, the variable i was declared using the let keyword: variables declared with the let (and const) keyword are block-scoped (a block is anything between { }). During each iteration, i will have a new value, and each value is scoped inside the loop.
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1);
+  }
+  
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1);
+  }
+// var and let
