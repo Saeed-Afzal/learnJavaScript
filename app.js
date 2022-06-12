029bd65f49a258f8f8652ad497535b2ac78e1315
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-06-12 18:30:46 +0500

    spread operators

--- app.js
+++ app.js
@@ -438,3 +438,14 @@
 // }
 //iterate object
 
+
+//spread operators
+// const arr1 = [1,2,3,4]
+// const arr2 = [5,6,7,8]
+
+// const newarr = [...arr1,...arr2] //1,2,3,4,5,6,7,8
+// const newarr = [..."abc"] //a,b,c
+
+// console.log(newarr);
+//spread operators
+
