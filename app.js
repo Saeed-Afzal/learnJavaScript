60194a7486fa8e20f5fe357a5c5d1a993a901828
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-08-19 20:33:32 +0500

    comment

--- app.js
+++ app.js
@@ -939,3 +939,5 @@ const products = [
 // 10. Write a JavaScript function which will take an array of numbers stored and find the second lowest and second greatest numbers, respectively.
 // Sample array : [1,2,3,4,5]
 // Expected Output : 2,4
+//
+//
